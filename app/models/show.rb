76fb94e7ffa14ed54de33569b209642dbe67cc6a
Marie (Cho) Hughes <kchohee0630@gmail.com>
2020-02-10 23:26:22 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,24 @@
-class AddSeasonToShows < ActiveRecord::Migration[5.2]
-  def change
-    add_column :shows, :season, :string
+class Show < ActiveRecord::Base
+  def Show::highest_rating
+    self.maximum(:rating)
+  end
+  def Show::most_popular_show
+    self.find_by(rating: highest_rating)
+  end
+  def Show::lowest_rating
+    self.minimum(:rating)
+  end
+  def self.least_popular_show
+    self.find_by(rating: lowest_rating)
+  end
+  def Show::ratings_sum
+    self.sum(:rating)
+  end
+  def Show::popular_shows
+    self.where("rating > 5")
+  end
+  def Show::shows_by_alphabetical_order
+    self.order(:name)
   end
 end
+© 2020 GitHub, In
